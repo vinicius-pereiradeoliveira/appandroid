c726ffeee6990711eb67c382f452ab40fdf050a4
vinicius.oliveira <vinicius.oliveira@cwi.com.br>
2017-04-10 18:03:26 -0300

    Implementa chamada de mapa
    Implementa chamada de mapa

--- app/src/main/java/appsind/atom/com/br/appandroidsind/InformationActivity.java
+++ app/src/main/java/appsind/atom/com/br/appandroidsind/InformationActivity.java
@@ -1,7 +1,10 @@
 package appsind.atom.com.br.appandroidsind;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import appsind.atom.com.br.appandroidsind.R;
 
@@ -11,5 +14,16 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+    }
+
+    public void getPhone(View view) {
+        Intent intent = new Intent(this, InformationActivity.class);
+        startActivity(intent);
+    }
+
+    public void getMap(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=Rua Apolinário de Moraes, 950 - Centro, Montenegro-RS"));
+        startActivity(intent);
     }
 }
